fedffff32a91b6c9cfa05238b61599a447273d1b
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-19 12:54:44 +0700

    refactor: map types

--- src/components/Todo/TodoPrivateList.tsx
+++ src/components/Todo/TodoPrivateList.tsx
@@ -3,6 +3,8 @@ import { gql, useQuery } from '@apollo/client';
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
 
+import { GetMyTodosQuery, Todos } from '../../generated/graphql';
+
 export const GET_MY_TODOS = gql`
   query getMyTodos {
     todos(where: {is_public: {_eq: false}}, order_by: {created_at: desc}) {
@@ -26,7 +28,7 @@ const TodoPrivateList = () => {
     data,     // An object containing the result of your GraphQL query. This will contain our actual data from the server. In our case, it will be the todo data.
     loading,  // A boolean that indicates whether the request is in flight. If loading is true, then the request hasn't finished. Typically this information can be used to display a loading spinner.
     error     // A runtime error with graphQLErrors and networkError properties. Contains information about what went wrong with your query.
-  } = useQuery(GET_MY_TODOS);
+  } = useQuery<GetMyTodosQuery>(GET_MY_TODOS);
 
   const filterResults = (filter: string): void => {
     setFilter(filter);
@@ -44,12 +46,12 @@ const TodoPrivateList = () => {
   
   let filteredTodos = data.todos;
   if (filter === "active") {
-    filteredTodos = data.todos.filter((todo: Todo) => todo.is_completed !== true);
+    filteredTodos = data.todos.filter((todo: Pick<Todos, "id" | "title" | "is_completed">) => todo.is_completed !== true);
   } else if (filter === "completed") {
-    filteredTodos = data.todos.filter((todo: Todo) => todo.is_completed === true);
+    filteredTodos = data.todos.filter((todo: Pick<Todos, "id" | "title" | "is_completed">) => todo.is_completed === true);
   }
 
-  const todoList = filteredTodos.map((todo: Todo, index: number) => (
+  const todoList = filteredTodos.map((todo: Pick<Todos, "id" | "title" | "is_completed">, index: number) => (
     <TodoItem
       key={'item'+index}
       index={index}
